0f6dab354b0db0ef3cba37ddfe9f0e749b52b19c
Amol Gholap <amol.gholap@neudesic.com>
2017-12-11 16:38:30 -0800

    Added code to upload file to blob

--- Blobs/Program.cs
+++ Blobs/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Blobs
 {
@@ -10,6 +12,20 @@ namespace Blobs
     {
         static void Main(string[] args)
         {
+            string storageConnection = System.Configuration.ConfigurationManager.AppSettings.Get("storgeAccountKey");
+            CloudStorageAccount stoAccount = CloudStorageAccount.Parse(storageConnection);
+
+            CloudBlobClient blobClient = stoAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference("objective2");
+
+            container.CreateIfNotExists();
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("examobjectives");
+            using (var fileStram = System.IO.File.OpenRead(@"Downloads\532_OD_Changes.pdf"))
+            {
+                blockBlob.UploadFromStream(fileStram);
+            }
         }
     }
 }
